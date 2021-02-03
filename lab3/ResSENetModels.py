161fc582cbdcf17081fc7ff5f3155ccf820fd26d
Sarach Rujiranurak <sarach@eruditetech.net>
2021-02-03 15:16:00 +0700

    add SE-Net

--- lab3/ResSENetModels.py
+++ lab3/ResSENetModels.py
@@ -4,6 +4,35 @@ from Resnet import ResNet
 def ResSENet18(num_classes = 10):
     return ResNet(ResidualSEBasicBlock, [2, 2, 2, 2], num_classes)
 
+def ResSENet50(num_classes = 10):
+    '''
+    First conv layer: 1
+    4 residual blocks with [3, 4, 6, 3] sets of three convolutions each: 3*3 + 4*3 + 6*3 + 3*3 = 48
+    last FC layer: 1
+    Total layers: 1+48+1 = 50
+    '''
+    return ResNet(ResidualSEBasicBlock, [3, 4, 6, 3], num_classes)
+
+
+def ResSENet101(num_classes = 10):
+    '''
+    First conv layer: 1
+    4 residual blocks with [3, 4, 23, 3] sets of three convolutions each: 3*3 + 4*3 + 23*3 + 3*3 = 99
+    last FC layer: 1
+    Total layers: 1+99+1 = 101
+    '''
+    return ResNet(ResidualSEBasicBlock, [3, 4, 23, 3], num_classes)
+
+
+def ResSENet152(num_classes = 10):
+    '''
+    First conv layer: 1
+    4 residual blocks with [3, 8, 36, 3] sets of three convolutions each: 3*3 + 8*3 + 36*3 + 3*3 = 150
+    last FC layer: 1
+    Total layers: 1+150+1 = 152
+    '''
+    return ResNet(ResidualSEBasicBlock, [3, 8, 36, 3], num_classes)
+
 if __name__ == '__main__':
     import torch
     net = ResSENet18()
